1826105ab4459a33fdb49c759b071ba5e8fd3dbd
juuliana <jumaju10@hotmail.com>
2021-03-16 00:59:43 -0300

    updated Home --

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router} from 'react-router-dom';
 import { Header, Description, Image } from './styles';
 import minions from '../../images/minions.png';
+import Products from '../Products';
+import Menu from '../../components/Menu';
 
 function Home() {
   return (
-    <Header>
+    <>
+      <Router>
+        <Menu/>
+      </Router>
+
+      <Header>
         <Description>
-            FAÇA JÁ A RESERVA DO SEU <text style={{color: '#FCE029'}}>BONECO MINIONS</text> EM MINIATURA!
+          FAÇA JÁ A RESERVA DO SEU <text style={{color: '#FCE029'}}>BONECO MINIONS</text> EM MINIATURA!
         </Description>
 
         <Image src={minions}></Image>
-    </Header>
+      </Header>
+
+      <Products/>
+    </>
   );
 }
 
