0de9b3c38b28d075a80020b692c710e4e386ffad
juuliana <jumaju10@hotmail.com>
2021-03-16 00:59:05 -0300

    App with Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router} from 'react-router-dom';
-import Menu from './components/Menu';
-import Home from './pages/Home';
-import Products from './pages/Products';
 import './global.css';
+
+import Routes from './routes';
 
 function App() {
   return (
     <>
-      <Router>
-        <Menu/>
-      </Router>
-     
-      <Home/>
-      <Products/>
+      <Routes/>
     </>
   );
 }
