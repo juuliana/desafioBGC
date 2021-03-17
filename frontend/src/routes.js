d89c801443f6085a057c0649f5bcfdbcda3bc4c3
juuliana <jumaju10@hotmail.com>
2021-03-16 23:28:59 -0300

    add Login in Routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter } from "react-router-dom";
 
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 const Routes = () => {
     return(
         <BrowserRouter>
             <Route component={Home} path="/" exact/>
+            <Route component={Login} path="/Login"/>
         </BrowserRouter>
     );
 }
