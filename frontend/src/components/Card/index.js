b1d22b6080d561654b0652077fe836c6c51114c9
juuliana <jumaju10@hotmail.com>
2021-03-18 13:51:51 -0300

    Context and Card

--- frontend/src/components/Card/index.js
+++ frontend/src/components/Card/index.js
@@ -1,27 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Container, ContainerImage, Image, Information, Title, Description, Button, Price } from './styles';
+import { ContextData } from '../../components/Context';
+import { Container, Header, ProductsGrid, ContainerImage, Image, Information, Title, Description, Button, Price } from './styles';
 
-export default function Card({title, imageUrl, body, price}){
-    return(
-        <Container>
-            <ContainerImage>
-                <Image src={imageUrl} alt='minions'/>
-            </ContainerImage>
+export class Card extends Component{
 
-            <Information>
-                <Title>
-                    <h2>{title}</h2>
-                </Title>
-                <Description>
-                    <p>{body}</p>
-                </Description>
-                <Price>
-                    <p>{price}</p>
-                </Price>
-            </Information>
+    static contextType = ContextData;
 
-            <Button as={Link} to="/ShoppingCart">Adicionar ao carrinho</Button>
-        </Container>
-    );
+    render(){
+        const {products} = this.context;
+
+        return(
+            <Header>
+                <ProductsGrid>
+                    {
+                        products.map(product => (
+                            
+                            <Container key={product.id}>
+                                <ContainerImage to={`/product/${product.id}`}>
+                                    <Image src={product.src} alt='minions'/>
+                                </ContainerImage>
+                            
+                                <Information>
+                                    <Title to={`/product/${product.id}`}>
+                                        <h2>{product.title}</h2>
+                                    </Title>
+                                    <Description to={`/product/${product.id}`}>
+                                        <p>{product.description}</p>
+                                    </Description>
+                                    <Price to={`/product/${product.id}`}>
+                                        <p>R${product.price}</p>
+                                    </Price>
+                                </Information>
+                            
+                                <Button onClick={()=> this.context.addShoppingCart(product.id)} as={Link} to="/ShoppingCart">Adicionar ao carrinho</Button>
+                            </Container>  
+                        ))
+                    }
+                </ProductsGrid>
+            </Header>
+        )
+    }
 }
+
+export default Card;
