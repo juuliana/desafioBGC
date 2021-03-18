bba0940263d1aa39cf052e263eda95ae28c57a00
juuliana <jumaju10@hotmail.com>
2021-03-18 13:40:48 -0300

    organize Code --Products

--- frontend/src/pages/Products/index.js
+++ frontend/src/pages/Products/index.js
@@ -1,33 +1,10 @@
 import React from 'react';
-import { Header, ProductsGrid } from './styles';
 
-import Card from '../../components/Card';
-import minions from '../../images/product1.jpg';
+import {Card} from '../../components/Card';
 
 function Products() {
   return (
-    <Header>
-      <ProductsGrid>
-        <Card
-          title='Minions remoto'
-          imageUrl={minions}
-          body='Leve já o seu minions remoto!'
-          price='R$100,00'
-        />
-        <Card
-          title='Minions remoto'
-          imageUrl={minions}
-          body='Leve já o seu minions remoto!'
-          price='R$100,00'
-        />
-        <Card
-          title='Minions remoto'
-          imageUrl={minions}
-          body='Leve já o seu minions remoto!'
-          price='R$100,00'
-        />
-      </ProductsGrid>
-    </Header>
+      <Card/>
   );
 }
 
