e09e6dccf74bfedf53f85f6b994f51947008774a
juuliana <jumaju10@hotmail.com>
2021-03-19 00:01:33 -0300

    add Routes --Register

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
 import Home from './pages/Home';
-import Login from './pages/Login';
+import Login from './pages/Login&Register/login';
+import Register from './pages/Login&Register/register';
 import ShoppingCart from './pages/ShoppingCart';
 
 export default function Routes(){
@@ -11,6 +12,7 @@ export default function Routes(){
             <Switch>
                 <Route component={Home} path="/" exact/>
                 <Route component={Login} path="/Login"/>
+                <Route component={Register} path="/Register"/>
                 <Route component={ShoppingCart} path="/ShoppingCart"/>      
             </Switch>
         </BrowserRouter>
