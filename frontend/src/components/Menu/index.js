c8f0672e74780950894b51bb5d5923dbd3d470e7
juuliana <jumaju10@hotmail.com>
2021-03-19 00:03:03 -0300

    update Menu -organize code

--- frontend/src/components/Menu/index.js
+++ frontend/src/components/Menu/index.js
@@ -4,15 +4,10 @@ import { Nav, NavLink } from './styles';
 export default function Menu(){
     return(
         <Nav>
-            <NavLink to='/'>
-                Home    
-            </NavLink>
-            <NavLink to='/Login'>
-                Login
-            </NavLink>
-            <NavLink to='/ShoppingCart'>
-                Carrinho
-            </NavLink>
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='/Login'>Login</NavLink>
+            <NavLink to='/Register'>Cadastrar</NavLink>
+            <NavLink to='/ShoppingCart'>Carrinho</NavLink>
         </Nav>
     );
 }
